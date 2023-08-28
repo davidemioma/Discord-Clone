f346afe76d93edf1b634dbeb430ac9857c96a479
david emioma <davidemiomauche@gmail.com>
2023-08-28 16:08:48 +0100

    Implemented theme toggle

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-[#313338] flex items-center justify-center w-screen h-screen overflow-hidden">
+    <div className="flex items-center justify-center w-screen h-screen overflow-hidden">
       {children}
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mode-toggle";
+
 export default function Home() {
-  return <div className="">Home</div>;
+  return (
+    <div className="">
+      <UserButton afterSignOutUrl="/" />
+
+      <ModeToggle />
+    </div>
+  );
 }
